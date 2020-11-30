f7293a81b9c181545185b75f18638ec8b2bdd06a
Chinmay Bharadwaj <55694209+Chinmaybharadwaj@users.noreply.github.com>
2020-11-30 21:45:50 +0530

    Update program.c

--- program.c
+++ program.c
@@ -3,14 +3,38 @@
 main()
 {
 
-float result;
+int result;
 int i;
-printf("enter 8 num");
+printf("enter 2 numbers");
+int a,b;
+scanf("%d%d",&a,&b);
+result= a+b;
+printf("result is %d",result);
+printf("enter 2 numbers");
+float a2,b2;
+scanf("%f%f",&a2,&b2);
+result= a/b;
+printf("result is %d",result);
+printf("enter 3 numbers");
+int c;
+scanf("%d%d%d",&a,&b,&c);
+printf("enter 3 numbers");
+float c2;
+scanf("%f%f%f",&a2,&b2,&c2);
+printf("enter 8 numbers");
+int a_int[20];
 for(i=0;i<8;i++)
 {
  scanf("%d",&a_int[i];
 }
+printf("enter 8 numbers");
+float a_float[20];
+for(i=0;i<8;i++)
+{
+ scanf("%f",&a_float[i];
+}
 result= a+b;
-printf("%d",result);
+result= a/b/c;
+printf("result is %d",result);
 return;
 }
